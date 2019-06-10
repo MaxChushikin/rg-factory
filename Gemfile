31d683565b2b443a7374e85ce8f8322e85e649b6
MaxChushikin <maxchushikin@gmail.com>
2019-06-10 16:21:43 +0300

    All tests passed

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@
 source 'https://rubygems.org'
 
 gem 'rspec'
+gem 'rubocop', '~> 0.70.0'
--- lib/factory.rb
+++ lib/factory.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # * Here you must define your `Factory` class.
 # * Each instance of Factory could be stored into variable. The name of this variable is the name of created Class
 # * Arguments of creatable Factory instance are fields/attributes of created class
@@ -14,3 +16,78 @@
 # - to_a
 # - values_at
 # - ==, eql?
+
+class Factory
+  class << self
+    def new(*fields, &methods)
+      if fields.first.is_a? String
+        class_name, *fields = fields
+        const_set(class_name, create_class(*fields, &methods))
+      else
+        create_class(*fields, &methods)
+      end
+    end
+
+    def create_class(*fields, &methods)
+      Class.new do
+        attr_accessor(*fields)
+
+        define_method :initialize do |*values|
+          raise ArgumentError, 'Inconsistency in the number of parameters ' if values.length != fields.length
+
+          fields.each_with_index do |field, index|
+            instance_variable_set "@#{field}", values[index]
+          end
+        end
+
+        define_method :[] do |field|
+          instance_variable_get field.is_a?(Integer) ? instance_variables[field] : "@#{field}"
+        end
+
+        define_method :[]= do |field, value|
+          instance_variable_set field.is_a?(Integer) ? instance_variables[field] : "@#{field}", value
+        end
+
+        define_method :each do |&block|
+          to_a.each(&block)
+        end
+
+        define_method :each_pair do |&block|
+          members.zip(to_a).each(&block)
+        end
+
+        define_method :dig do |*keys|
+          keys.inject(self) { |values, key| values[key] if values }
+        end
+
+        define_method :size do
+          instance_variables.size
+        end
+        alias_method :length, :size
+
+        define_method :members do
+          fields
+        end
+
+        define_method :select do |&block|
+          to_a.select(&block)
+        end
+
+        define_method :to_a do
+          instance_variables.map { |value| instance_variable_get value }
+        end
+
+        define_method :values_at do |*indexes|
+          to_a.values_at(*indexes)
+        end
+
+        define_method :== do |other|
+          self.class == other.class && to_a == other.to_a
+        end
+        alias_method :eql?, :==
+
+        class_eval &methods if block_given?
+      end
+    end
+  end
+end
